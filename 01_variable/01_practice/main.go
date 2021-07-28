964d364c94116d5dc3e58081359e553f759f8d43
GoesToTwentyOne <nihadhossainthpibd373@gmail.com>
2021-07-28 22:08:09 +0600

    all about variable

---
+++ 01_variable/01_practice/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//package level variable {if the identifier Upper case - it is applicable for out use out side that package or if the identifier Lower  case not aplicable for exported.Just unexported  }
+var i int = 23
+
+func main() {
+	fmt.Println(i)
+	//variable shadowing
+	var i int = 27
+	fmt.Println(i)
+	//reassign value
+	i = 54
+	fmt.Println(i)
+
+	// type conversion
+	var j float64
+	j = float64(i)
+	fmt.Printf("%T %v \n", j, j)
+	//string conversion convention
+	var x int = 25
+	var s string
+	s = string(x) // print astric as don't detect ascii value
+	fmt.Printf("%T %v \n", s, s)
+	s = strconv.Itoa(x)
+	fmt.Printf("%T %v \n", s, s)
+
+}
